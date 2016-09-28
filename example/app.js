c094138101e62b898f839d8037983020b0dc3262
kknts <kyawnainghtun93@gmail.com>
2016-09-28 22:28:47 +0630

    added zawgyi detector

--- example/app.js
+++ example/app.js
@@ -20,6 +20,7 @@ var converterApp = angular.module('converterApp', ['ui.router', 'ngZ2UConverter'
       var vm = this;
       vm.name = "Converter Application";
       vm.zgText = "ျမန္မာယူနီကုဒ္ႏွင့္ ပါတ္သက္ေသာ သတင္းမ်ား၊ ေဆာင္းပါးမ်ားႏွင့္ ျမန္မာယူနီကုဒ္ဧရိယာ အဖြဲ႕၏ လႈပ္ရွား ေဆာင္ရြက္မႈမ်ားကို www.mmunicode.org တြင္ တင္ျပေပးသြားမွာ ျဖစ္ပါတယ္။";
+      // vm.zgText = "ကောင်းသောနေ့ခင်းလေးပါ";
       vm.filterByController = function(){
         vm.filterByContrlResult = $filter('convertToUnicode')(vm.zgText);
       }
